ba073109e4d45b10133c1f37a95b2271a91ec57f
vpletuhin <qwertyzmail@gmail.com>
2017-09-07 20:51:54 +0300

    openshift v3 configuration

--- config/index.js
+++ config/index.js
@@ -1,4 +1,23 @@
 const env = process.env.NODE_ENV || 'development',
     cfg = require('./config')[env];
 
-module.exports = cfg;
+const apply_env_params = (cfg) => {
+    const result = {};
+    for (let [key, value] of entries(cfg)) {
+        if (typeof value === 'string' && value.startsWith('${') && value.endsWith('}')) {
+            const param = value.substring(2, value.length - 1);
+            result[key] = env[param] || value;
+        } else
+            result[key] = value;
+    }
+
+    return result;
+};
+
+function* entries(obj) {
+    for (let key of Object.keys(obj)) {
+        yield [key, obj[key]];
+    }
+}
+
+module.exports = apply_env_params(cfg);
